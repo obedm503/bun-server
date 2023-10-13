7eb6a16ca2298e8711998524041b81527322608a
Obed Miranda <obed.miranda@transova.com>
2023-10-13 12:00:05 -0500

    flatten headers

--- index.ts
+++ index.ts
@@ -1,15 +1,36 @@
+function flatten(ob: any) {
+  const result = {} as any;
+
+  for (const i in ob) {
+    if (typeof ob[i] === 'object' && !Array.isArray(ob[i])) {
+      const temp = flatten(ob[i]);
+      for (const j in temp) {
+        // Store temp in result
+        result[i + '.' + j] = temp[j];
+      }
+    } else {
+      result[i] = ob[i];
+    }
+  }
+  return result;
+}
+
 const server = Bun.serve({
   async fetch(req) {
     console.log(
-      JSON.stringify({
-        url: req.url,
-        method: req.method,
-        mode: req.mode,
-        headers: req.headers,
-        credentials: req.credentials,
-        destination: req.destination,
-        body: await req.text(),
-      }),
+      flatten(
+        JSON.parse(
+          JSON.stringify({
+            url: req.url,
+            method: req.method,
+            mode: req.mode,
+            credentials: req.credentials,
+            destination: req.destination,
+            body: await req.text(),
+            headers: req.headers,
+          }),
+        ),
+      ),
     );
     return new Response('ok');
   },
